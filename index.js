7a12ccea6fc6b89608b7570329ca02afa0651c55
Harshad Prajapati <iamharshad.prajapati@gmail.com>
2022-03-09 01:55:42 +0530

    added get route

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.get("/about-us", function (req, res) {
+  res.send("About us");
+});
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello World - Home");
 });
 
 app.listen("8866");
